419deb3fbd038a7216cf55c56e6bb7d039e4f40c
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-06-26 08:33:43 -0600

    WIP - added create_integer_array method; working on double_odd_indices method.

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,6 +1,30 @@
 require 'pry'
 
 class CreditCheck
-  
+
+  attr_reader   :cc_number,
+                :cc_split
+
+  def initialize(cc_number)
+    @cc_number = cc_number
+    @cc_split = []
+  end
+
+  def create_integer_array
+    @cc_split = cc_number.split(//).map! do |digit|
+      digit.to_i
+    end
+  end
+
+  def double_odd_indices
+    @cc_split.map! do |digit|
+      if @cc_split.index(digit).odd? == true
+        digit * 2
+      else
+        digit
+      end
+    end
+    # return @cc_split
+  end
 
 end
--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -6,8 +6,30 @@ require './lib/credit_check'
 class CreditCheckTest < Minitest::Test
 
   def test_it_exists
-    credit_check = CreditCheck.new
+    credit_check = CreditCheck.new("1234567887654321")
 
     assert_instance_of CreditCheck, credit_check
   end
+
+  def test_it_has_cc_number
+    credit_check = CreditCheck.new("1234567887654321")
+
+    assert_equal "1234567887654321", credit_check.cc_number
+  end
+
+  def test_it_converts_string_into_array_of_integers
+    credit_check = CreditCheck.new("2468")
+
+    assert_equal [], credit_check.cc_split
+
+    assert_equal [2, 4, 6, 8], credit_check.create_integer_array
+  end
+
+  def test_it_doubles_every_odd_index_digit
+    credit_check = CreditCheck.new("1111")
+    credit_check.create_integer_array
+
+    assert_equal [1, 2, 1, 2], credit_check.double_odd_indices
+  end
+
 end
